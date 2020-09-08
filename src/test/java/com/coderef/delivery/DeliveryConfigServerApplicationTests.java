de02f7dd8c638f965664b0fe291247fe21ea590f
vitorprocopio <vitor.nprocopio@gmail.com>
2020-09-07 21:59:19 -0300

    first comit

---
+++ src/test/java/com/coderef/delivery/DeliveryConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderef.delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
